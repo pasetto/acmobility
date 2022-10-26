34aab1b74b925fb9266d2fcd8a4bd3ea5e281ecb
Eduardo Pasetto <eduardo@nanodesign.com.br>
2022-10-26 19:04:34 -0300

    first commit

---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,46 @@
+/* tslint:disable:max-line-length */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Dashboard',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'settings',
+        title: 'Configurações',
+        type : 'basic',
+        icon : 'heroicons_outline:cog',
+        link : '/settings'
+    }
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
---
+++ src/app/modules/admin/pages/settings/account/account.component.ts
@@ -0,0 +1,47 @@
+import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+
+@Component({
+    selector       : 'settings-account',
+    templateUrl    : './account.component.html',
+    encapsulation  : ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SettingsAccountComponent implements OnInit
+{
+    accountForm: UntypedFormGroup;
+
+    /**
+     * Constructor
+     */
+    constructor(
+        private _formBuilder: UntypedFormBuilder
+    )
+    {
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * On init
+     */
+    ngOnInit(): void
+    {
+        // Create the form
+        this.accountForm = this._formBuilder.group({
+            name    : ['Nome do Usuário'],
+            username: ['brianh'],
+            cpf     : ['000.000.000-00'],
+            rg      : ['00.000.000-0'],
+            title   : ['Senior Frontend Developer'],
+            company : ['YXZ Software'],
+            about   : ['Hey! This is Brian; husband, father and gamer. I\'m mostly passionate about bleeding edge tech and chocolate! 🍫'],
+            email   : ['email@exemplo.com', Validators.email],
+            phone   : ['(00) 0000-0000'],
+            country : ['brazil'],
+            language: ['english']
+        });
+    }
+}
